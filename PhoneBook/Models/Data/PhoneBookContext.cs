8e51cb51d6ca6496d3c4ab071884d560a1e8515d
Ismail M <im_077@hotmail.com>
2022-08-22 11:12:42 +0200

    Add project files.

---
+++ PhoneBook/Models/Data/PhoneBookContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Models.Data
+{
+    public class PhoneBookContext : DbContext
+    {
+        public DbSet<Contacts> Contacts { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=(localdb)\\Local;Initial Catalog=PhoneBook; Integrated Security=True");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\Local;Database=PhoneBook;Trusted_Connection=True;");
+        }
+    }
+}
